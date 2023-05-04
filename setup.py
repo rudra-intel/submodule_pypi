7d901c4b482cbed866c1ac090cea4962286b5367
rudra-intel <111494235+rudra-intel@users.noreply.github.com>
2023-05-04 10:49:38 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 import setuptools
 setuptools.setup(
         name="Guesser_game",
-        version="0.1.1",
+        version="10.11.12",
         author="Vinutha",
         Description="This is Guess gamme package",
         packages=["Guesser_game"]
